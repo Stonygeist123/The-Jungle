f82b1dde8d82fb743af9255db7e25a00769b3149
timplay33 <tim3play3@gmail.com>
2023-03-06 18:25:18 +0100

    modified:   Assets/Game/Game.unity 	modified:   Assets/Game/Scripts/GameManager.cs

--- Assets/Game/Scripts/GameManager.cs
+++ Assets/Game/Scripts/GameManager.cs
@@ -4,5 +4,5 @@ public class GameManager : MonoBehaviour
 {
     public Transform player;
     public Vector3 spawnPosition = new Vector3(250f, 52f, 250f);
-    void Start() => player.localPosition = new(250f, 52f, 250f);
+    void Start() => player.localPosition = spawnPosition;
 }
